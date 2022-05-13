11484ae2d69ff87c932d678586f70a5d030eee33
SmithaVerity <98273657+SmithaVerity@users.noreply.github.com>
2022-05-14 02:37:11 +0530

    Update deployment.sh

--- deployment.sh
+++ deployment.sh
@@ -2,5 +2,5 @@ echo "deb https://deb.gremlin.com/ release non-free" | sudo tee /etc/apt/sources
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 9CDB294B29A5B1E2E00C24C022E8EF3461A50EF6
 sudo apt-get update && sudo apt-get install -y gremlin gremlind
 gremlin init
-# Replace with your Key and secret key from app.gremlin.com
+# Enter your Key and secret key from app.gremlin.com
 
